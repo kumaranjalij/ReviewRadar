1e186b10814d83c6c0ec5bf6c81575e544e2398e
Anjali J Kumar <kumaranjalij@gmail.com>
2024-05-17 11:16:34 +0530

    frontend build

--- frontend/reviewradar/src/App.js
+++ frontend/reviewradar/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import CompanyDashboard from "./Pages/CompanyDashboard";
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
